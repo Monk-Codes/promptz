b0c65c2557e70f30e8df49f1ac9a945e72395130
monk-codes <your_donotreply_email_id>
2024-05-09 14:08:35 +0530

    CRUD

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,11 +1,12 @@
 "use client";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import Profile from "@components/profile";
 const MyProfile = () => {
  const { data: session } = useSession();
  const [posts, setPosts] = useState([]);
+ const router = useRouter();
  useEffect(() => {
   const fetchPosts = async () => {
    const response = await fetch(`/api/users/${session?.user.id}/posts`);
@@ -15,9 +16,23 @@ const MyProfile = () => {
   if (session?.user.id) fetchPosts();
  });
 
- const handleEdit = () => {};
- const handleDelete = () => {};
+ const handleEdit = (post) => {
+  router.push(`/update-prompt?id=${post._id}`);
+ };
+ const handleDelete = async (post) => {
+  const hasConfirmed = confirm(`Are you sure you want to delete this prompt ?`);
+  if (hasConfirmed) {
+   try {
+    await fetch(`/api/prompt/${post._id.toString()}`, {
+     method: "DELETE",
+    });
+    const filteredPosts = posts.filter((e) => e._id == post._id);
+    setPosts(filteredPosts);
+   } catch (error) {
+    console.log(error);
+   }
+  }
+ };
  return <Profile name="My" desc="Welcome to your profile" data={posts} handleEdit={handleEdit} handleDelete={handleDelete}></Profile>;
 };
-
 export default MyProfile;
